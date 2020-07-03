1e1fecca8e8c9a182ce0fce435cb9a2b22305eac
Luke <luk.tex37@gmail.com>
2020-07-03 12:37:37 +0100

    Added a progress indicator

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 
 int main()
 {
-    const int image_width = 256;
-    const int image_height = 256;
+    const int image_width = 25600;
+    const int image_height = 25600;
 
     std::cout << "P3\n"
               << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; j--)
     {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
             auto r = double(i) / (image_width - 1);
@@ -25,4 +26,6 @@ int main()
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
